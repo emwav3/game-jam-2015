62ae31386b87a8016cc5758b313f348f452ec143
kylescott <ky_scotty4@Hotmail.com>
2015-03-07 14:21:14 -0600

    Added Sah Gar's scene

---
+++ game-jam-2015/Assets/scripts/GameState.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameState : MonoBehaviour {
+	public int score;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ game-jam-2015/Assets/scripts/PlayerCollision.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D coll){
+		//If player collides with the coin
+		if (coll.gameObject.tag == "Coin") {
+				
+		}
+	}
+}
