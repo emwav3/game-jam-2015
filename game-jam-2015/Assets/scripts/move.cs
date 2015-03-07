c67c8b058e8428848ee3f054f176e096eef6f032
kylescott <ky_scotty4@Hotmail.com>
2015-03-07 00:55:36 -0600

    retry

--- game-jam-2015/Assets/scripts/move.cs
+++ game-jam-2015/Assets/scripts/move.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class move : MonoBehaviour {
 	public static float distanceTraveled;
+	public float speed = 5f;
+	public float jumpPower = 100f;
+	public bool grounded = true;
+	public bool hasJumped = false;
 	// Use this for initialization
 	void Start () {
 	
@@ -10,7 +14,26 @@ public class move : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Translate(5f * Time.deltaTime, 0f, 0f);
+//		transform.Translate(5f * Time.deltaTime, 0f, 0f);
+		//For moving left/right
+		float motion = Input.GetAxis ("Horizontal");
+		rigidbody2D.velocity = new Vector2 (motion * speed, rigidbody2D.velocity.y);
+		//For jumping
+		if(!grounded && rigidbody2D.velocity.y == 0) {
+			grounded = true;
+		}
+		if (Input.GetButtonDown("Vertical") && grounded == true) {
+			hasJumped = true;
+		}
+
 		distanceTraveled = transform.localPosition.x;
+	}
+	void FixedUpdate (){
+		//If the character has jumped
+		if(hasJumped){
+			rigidbody2D.AddForce(transform.up*jumpPower);
+			grounded = false;
+			hasJumped = false;
+		}
 	}
 }
