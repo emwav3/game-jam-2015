f6f0466dfcdc16a6b586d6171dbb0c1719eecd7b
Lawrence Foley <emwav333@gmail.com>
2015-03-07 00:45:45 -0600

    Made everything

--- game-jam-2015/Assets/scripts/sceneManager.cs
+++ game-jam-2015/Assets/scripts/sceneManager.cs
@@ -5,7 +5,7 @@ public class sceneManager : MonoBehaviour {
 	
 	public Transform prefab;
 	public int numberOfObjects;
-	public float recycleOffset;
+	public float offset;
 	public Vector3 startPosition;
 	
 	private Vector3 nextPosition;
@@ -17,24 +17,24 @@ public class sceneManager : MonoBehaviour {
 		for (int i = 0; i < numberOfObjects; i++) {
 			Transform o = (Transform)Instantiate(prefab);
 			o.localPosition = nextPosition;
-			nextPosition.x += 7.35f;
+			nextPosition.x += offset;
 			objectQueue.Enqueue(o);
 		}
 	}
 	
 	void Update () {
-		if (objectQueue.Peek().localPosition.x + recycleOffset < move.distanceTraveled) {
-			Transform o = objectQueue.Dequeue();
+		if (objectQueue.Peek().localPosition.x + offset * 2 < move.distanceTraveled) {
+			nextPosition.x = objectQueue.Peek().localPosition.x + offset * 4;
+			Transform trash = objectQueue.Dequeue();
+			GameObject t = trash.gameObject;
+			Destroy(t);
+			//Transform o = objectQueue.Dequeue();
+			//o.localPosition = nextPosition;
+			//nextPosition.x += o.localScale.x;
+			Transform o = (Transform)Instantiate(prefab);
 			o.localPosition = nextPosition;
-			nextPosition.x += o.localScale.x;
 			objectQueue.Enqueue(o);
 		}
 	}
-
-	private void Recycle () {
-		Transform o = objectQueue.Dequeue();
-		o.localPosition = nextPosition;
-		nextPosition.x += o.localScale.x;
-		objectQueue.Enqueue(o);
-	}
+	
 }
