cbdcdfa6e5a80a1fb4fbff87b4159d61c4de197f
Lawrence Foley <emwav333@gmail.com>
2015-03-07 16:38:29 -0600

    Linked score to GUI score

--- game-jam-2015/Assets/scripts/GameState.cs
+++ game-jam-2015/Assets/scripts/GameState.cs
@@ -1,16 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.UI;
 public class GameState : MonoBehaviour {
 	public int points;
 
 	// Use this for initialization
 	void Start () {
-	
+		points = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		GameObject guiPoints = GameObject.FindGameObjectWithTag ("guiPoints");
+		Text pointsText =guiPoints.GetComponent<Text>();
+		pointsText.text = "Score: " + points.ToString();
 	}
 }
