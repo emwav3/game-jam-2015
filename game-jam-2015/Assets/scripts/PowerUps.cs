f47510c71e845e39192937504ef6172fb38e9b3a
Lawrence Foley <emwav333@gmail.com>
2015-03-08 15:31:28 -0500

    Rockets destroy on hit

--- game-jam-2015/Assets/scripts/PowerUps.cs
+++ game-jam-2015/Assets/scripts/PowerUps.cs
@@ -6,12 +6,24 @@ public class PowerUps : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		string name = this.gameObject.name;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		//rigidbody2D.velocity = transform.up.normalized * speed;
 		transform.Translate(speed * Time.deltaTime, 0f, 0f);
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+		GameObject me = this.gameObject;
+		if (name == "missile") {
+			Destroy (me);
+			//play explosion here
+		} else {
+			if(col.gameObject.tag != "ground"){
+				Destroy(me);
+			}
+		}
 	}
 }
