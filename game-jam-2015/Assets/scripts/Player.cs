3fbf215022d8cc59d5164c8cd0432d58e5254fe7
kylescott <ky_scotty4@Hotmail.com>
2015-03-07 23:08:09 -0600

    Put the PlayerCollision script into Player Script

--- game-jam-2015/Assets/scripts/Player.cs
+++ game-jam-2015/Assets/scripts/Player.cs
@@ -4,6 +4,10 @@ using System.Collections;
 public class Player : MonoBehaviour {
 	public bool hasFireball;
 	public bool hasRocket;
+	public GameState points;
+	public GameObject[]  powerUps;
+	public GameObject powerUp;
+	int randomNum;
 
 	// Use this for initialization
 	void Start () {
@@ -21,4 +25,26 @@ public class Player : MonoBehaviour {
 			hasRocket = false;
 		}
 	}
+
+	void OnTriggerEnter2D(Collider2D collider){
+//		playerScript = (Player)GameObject.FindGameObjectWithTag("player").GetComponent<Player>();
+				points = (GameState)GameObject.FindGameObjectWithTag ("stateManager").GetComponent<GameState> ();
+				//		GameState gameStateScript =(GameState)points.GetComponent<GameState> ();
+				//If player collides with the coin
+				if (collider.gameObject.tag == "coin") {
+						points.points += 1;	
+						Destroy (collider.gameObject);
+				}
+				//If the player collides with a powerUp box
+				if (collider.gameObject.tag == "powerUp") {
+						randomNum = Random.Range (0, powerUps.Length);
+						powerUp = powerUps [randomNum];
+						Debug.Log (powerUp.name.ToString ());
+						if (powerUp.name == "fireball") {
+								hasFireball = true;
+						} else if (powerUp.name == "rocket") {
+								hasRocket = true;
+						}
+				}
+		}
 }
